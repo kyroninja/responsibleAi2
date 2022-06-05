86d8b80364270c37c7165f4a220198ac54cc9f7e
kyroninja <53964950+kyroninja@users.noreply.github.com>
2022-06-05 08:56:29 +0200

    commit2

--- responsibleAi2/settings.py
+++ responsibleAi2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9f6220a5-fa79-4436-a121-d5535ee9cf37'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
